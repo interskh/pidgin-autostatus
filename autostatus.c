8adf3a8193d4af703d0c0b30f2128581b1f1caa7
Kyle Sun <interskh@gmail.com>
2009-10-01 23:04:57 -0400

    + License

--- autostatus.c
+++ autostatus.c
@@ -1,16 +1,21 @@
 /*
- autostatus - plugin to set status based on the user's ip address
+ autostatus - plugin to set status based on the user's location,
+ namely, the ip address
+ Copyright (C) 2009  Kyle Sun <interskh@gmail.com>
 
- Copyright (c) 2009, Kyle Sun <interskh@gmail.com>
- All rights reserved.
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
 
- Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
- * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
- * The name of the author may not be used to endorse or promote products derived from this software without specific prior written permission.
-
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc.
 
 */
 
@@ -367,7 +372,7 @@ set_status_all()
       guint32 fit = 0;
       gboolean found = FALSE;
       for (i=0; i<rule_cnt; i++){
-         if ((rules[i].netmask > max_len) && \
+         if ((rules[i].netmask >= max_len) && \
                (((myip ^ rules[i].ip) & (~0 << (32-rules[i].netmask))) == 0)) {
             max_len = rules[i].netmask;
             fit = i;
@@ -446,7 +451,7 @@ static PurplePluginInfo info = {
 	"Auto Status Plugin",
 	"Auto Status Plugin",
 	"Kyle Sun <interskh@gmail.com>", /* correct author */
-	"http://",
+	"http://xxx.xxx",
 
 
 	plugin_load,
