90c88e48a2cc95c4faa65a5979f212d00a941a27
Kyle Sun <interskh@gmail.com>
2009-10-06 10:38:00 -0400

    fix) no need for actions

--- autostatus.c
+++ autostatus.c
@@ -273,42 +273,6 @@ PurplePluginPrefFrame *get_plugin_pref_frame(PurplePlugin *plugin) {
  * plugin_action_test_cb() callback function */
 PurplePlugin *autostatus_plugin = NULL;
 
-/* This function is the callback for the plugin action we added. All we're
- * doing here is displaying a message. When the user selects the plugin
- * action, this function is called. */
-static void
-plugin_action_test_cb (PurplePluginAction * action)
-{
-	purple_notify_message (autostatus_plugin, PURPLE_NOTIFY_MSG_INFO,
-		"Plugin Actions Test", "This is a plugin actions test :)", NULL, NULL,
-		NULL);
-}
-
-/* we tell libpurple in the PurplePluginInfo struct to call this function to
- * get a list of plugin actions to use for the plugin.  This function gives
- * libpurple that list of actions. */
-static GList *
-plugin_actions (PurplePlugin * plugin, gpointer context)
-{
-	/* some C89 (a.k.a. ANSI C) compilers will warn if any variable declaration
-	 * includes an initilization that calls a function.  To avoid that, we
-	 * generally initialize our variables first with constant values like NULL
-	 * or 0 and assign to them with function calls later */
-	GList *list = NULL;
-	PurplePluginAction *action = NULL;
-
-	/* The action gets created by specifying a name to show in the UI and a
-	 * callback function to call. */
-	action = purple_plugin_action_new ("Plugin Action Test", plugin_action_test_cb);
-
-	/* libpurple requires a GList of plugin actions, even if there is only one
-	 * action in the list.  We append the action to a GList here. */
-	list = g_list_append (list, action);
-
-	/* Once the list is complete, we send it to libpurple. */
-	return list;
-}
-
 static gboolean
 set_status (PurpleAccount *acnt, const char *loc)
 {
@@ -464,8 +428,8 @@ static PurplePluginInfo info = {
 	NULL,
 	NULL,
 	&plugin_prefs,
-	plugin_actions,		/* this tells libpurple the address of the function to call
-				   to get the list of plugin actions. */
+   NULL,
+
 	NULL,
 	NULL,
 	NULL,
